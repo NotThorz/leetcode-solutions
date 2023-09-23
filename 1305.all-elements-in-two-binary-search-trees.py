b0b20178fcf31af1f6bd5f4470581a0640471822
NotThorz <reda45696@gmail.com>
2023-09-23 19:51:35 +0100

    23/09

---
+++ 1305.all-elements-in-two-binary-search-trees.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=1305 lang=python3
+#
+# [1305] All Elements in Two Binary Search Trees
+#
+
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        # O(n+m)
+        l1 = []
+        l2 = []
+
+        def inorder(node, l):
+            if not node:
+                return
+            inorder(node.left, l)
+            l.append(node.val)
+            inorder(node.right, l)
+
+        inorder(root1, l1)
+        inorder(root2, l2)
+
+        def merge(x, y):
+            ans = []
+            i = 0
+            j = 0
+            while i < len(x) and j < len(y):
+                if x[i] <= y[j]:
+                    ans.append(x[i])
+                    i += 1
+                else:
+                    ans.append(y[j])
+                    j += 1
+            ans += x[i:]
+            ans += y[j:]
+            return ans
+
+        return merge(l1, l2)
+
+
+# @lc code=end
