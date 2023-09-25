f9891f56b043615331653d614abf8bd7b294d965
NotThorz <reda45696@gmail.com>
2023-09-25 13:10:46 +0100

    25/09

--- 389. Fidn the Difference.py
+++ 389. Fidn the Difference.py
@@ -1,11 +1,8 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         s=Counter(s)
-        d=Counter(t)
+        t=Counter(t)
 
-        for char in t :
-            if char not in  s:
-                return char
-            else:
-                if d[char]!=s[char]:
-                    return char 
+        for char,v in t.items() :
+            if v!=s[char]:
+                return char 
